b05575ead3b76869d84551ebd036bff841b19ede
Fajar Hari Prabowo <me.fhprabowo@gmail.com>
2023-02-02 12:32:19 +0700

    style: add some comments

--- install.sh
+++ install.sh
@@ -1,6 +1,13 @@
+# flatpak from flathub
+# -------------------------------------------------
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
 # rpmfusion
+# -------------------------------------------------
 sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
 sudo dnf groupupdate core -y
+
+## rpmfusion - multimedia
 
 # additional multimedia codec
 sudo dnf groupupdate multimedia --setop="install_weak_deps=False" --exclude=PackageKit-gstreamer-plugin -y
@@ -17,14 +24,17 @@ sudo dnf swap mesa-vdpau-drivers mesa-vdpau-drivers-freeworld -y
 sudo dnf install rpmfusion-nonfree-release-tainted -y
 sudo dnf --repo=rpmfusion-nonfree-tainted install "*-firmware" -y
 
-# visual studio code
+# apps
+# -------------------------------------------------
+
+# apps - visual studio code
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 dnf check-update -y
 sudo dnf install code -y
 
-# libnsl for xampp dependency
-sudo dnf install libnsl -y
+# dependencies fixes
+# -------------------------------------------------
 
-# flatpak
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+# libnsl for xampp
+sudo dnf install libnsl -y
